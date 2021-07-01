f955ef3517c87018d53f6c500f06e8ebf16507cf
Aleksandr Moiseev <aimoiseev@artlebedev.ru>
2021-07-01 16:01:34 +0300

    stage 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-//import html2canvas from 'html2canvas';
 require('codemirror/mode/xml/xml');
 require('codemirror/mode/javascript/javascript');
 require('codemirror/mode/css/css');
@@ -7,15 +6,11 @@ import CodeMirror from 'codemirror';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    //const targetArea = document.querySelector('.preview__text');
-    const saveButton = document.querySelector('.preview__button');
-    const refreshPresetsButton = document.querySelector('.presets-block__button-refresh');
+    const saveButton = document.querySelector('.save__button');
     const presetsList = document.querySelector('.presets-block__list');
 
-    //const resultBlock = document.querySelector('.result-block');
-
     let doc = document.querySelector('#test').contentWindow.document;
-    const presetTitle = document.querySelector('#title-text');
+    const presetTitle = document.querySelector('.block__code-menu-preset-title');
 
 
     var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
@@ -37,81 +32,50 @@ document.addEventListener('DOMContentLoaded', () => {
           doc.close();
       })
 
+    const refreshPresetsList = () => {
+      const formData = new FormData();
+      formData.append('action', 'refresh');
+      const options = {
+          method: 'POST',
+          body: formData,
+      };
+      fetch('./index.php', options)
+      .then(res => res.json())
+      .then(res => {
+          presetsList.innerHTML = '';
+          res.forEach(preset => {
+              presetsList.innerHTML += `
+                  <input type="radio" id="preset${preset.id}" name="preset" value="${preset.id}">
+                  <label for="preset${preset.id}">${preset.title}</label>
+              `;
+          });
+      });
+    };
+
+    refreshPresetsList()
+
     saveButton.addEventListener('click', () => {
         //console.log(doc.activeElement.innerHTML)
-            const presetHTML = doc.activeElement.innerHTML;
-            const formData = new FormData();
-            formData.append('html', presetHTML);
-            formData.append('title', presetTitle.value);
-            formData.append('action', 'save');
-            const options = {
-                method: 'POST',
-                body: formData,
-              };
-            fetch('./index.php', options)
-            .then(res => res.text())
-            .then(res => console.log(res))
-        
-            /* === Выгрузка изображения с помощью html2canvas ====
-        
-        html2canvas(doc, {
-            //useCORS: true,
-            scale:20
-        })
-        .then(function (canvas) {
-            const canvasImg = canvas.toDataURL("image/jpg");
-            console.log(canvas)
-            resultBlock.innerHTML += `<img src="${canvasImg}" style="border: 1px solid #000000" alt="">`
-            resultBlock.style.display = 'block';
-        
-
-        });
-        
-    */
-    });
-/*
-    const formData = new FormData();
-        formData.append('action', 'refresh');
-        const options = {
-            method: 'POST',
-            body: formData,
-          };
-        fetch('./index.php', options)
-        .then(res => res.json())
-        .then(res => {
-            presetsList.innerHTML = '';
-            res.forEach(preset => {
-                presetsList.innerHTML += `
-                    <input type="radio" id="preset${preset.id}" name="preset" value="${preset.id}">
-                    <label for="preset${preset.id}">${preset.title}</label>
-                `;
-            });
-        })
-*/
-    refreshPresetsButton.addEventListener('click', () => {
-        //console.log(doc.activeElement.innerHTML)
+        const presetHTML = doc.activeElement.innerHTML;
         const formData = new FormData();
-        formData.append('action', 'refresh');
+        formData.append('html', presetHTML);
+        formData.append('title', presetTitle.value);
+        formData.append('action', 'save');
         const options = {
             method: 'POST',
             body: formData,
-          };
+        };
         fetch('./index.php', options)
-        .then(res => res.json())
+        .then(res => res.text())
         .then(res => {
-            presetsList.innerHTML = '';
-            res.forEach(preset => {
-                presetsList.innerHTML += `
-                    <input type="radio" id="preset${preset.id}" name="preset" value="${preset.id}">
-                    <label for="preset${preset.id}">${preset.title}</label>
-                `;
-            });
+            console.log(res);
+            refreshPresetsList();
         })
-    })
+    });
+
 
     presetsList.addEventListener('click', e => {
         if (e.target.type === "radio") {
-            console.log(e.target);
 
             const formData = new FormData();
             formData.append('action', 'load');
