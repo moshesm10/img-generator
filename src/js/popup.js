1debfcd16f139293c1b4d6efaa402eba26aeeca6
Aleksandr Moiseev <aimoiseev@artlebedev.ru>
2021-07-30 12:17:57 +0300

    Исправил баг с неактуальными событиями на кнопках

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,23 +1,37 @@
 export const displayPopup = (selector, title, text, action, actionFunction, parameter) => {
+    // Очистка старых кнопок
+    selector.removeChild(selector.lastElementChild);
+    
     selector.children[0].innerText = title;
     selector.children[1].innerText = text;
 
-    const buttonCencel = selector.lastElementChild.children[0]
-    const buttonAccept = selector.lastElementChild.lastElementChild;
+    const popupAcceptButtons = document.createElement('div');
+    popupAcceptButtons.classList.add('popup-accept__buttons');
+
+    const buttonCencel = document.createElement('button');
+    buttonCencel.classList.add('popup-accept__button', 'popup-accept__button-cancel');
+    buttonCencel.textContent = 'Отмена';
+    popupAcceptButtons.append(buttonCencel);
+
+    const buttonAccept = document.createElement('button');
+    buttonAccept.classList.add('popup-accept__button', 'popup-accept__button-accept')
+    popupAcceptButtons.append(buttonAccept);
+
+    selector.append(popupAcceptButtons);
 
     switch (action) {
         case 'post':
-            buttonAccept.innerText = 'Добавить';
+            buttonAccept.textContent = 'Добавить';
             buttonAccept.style.color = '#2ECC71';
 
             break;
         case 'remove post':
-            buttonAccept.innerText = 'Изъять';
+            buttonAccept.textContent = 'Изъять';
             buttonAccept.style.color = '';
 
             break;
         case 'delete print':
-            buttonAccept.innerText = 'Удалить';
+            buttonAccept.textContent = 'Удалить';
             buttonAccept.style.color = '';
 
             break;
@@ -30,5 +44,6 @@ export const displayPopup = (selector, title, text, action, actionFunction, para
         selector.classList.add('hide');
     }, {once: true});
     buttonCencel.addEventListener('click', () => selector.classList.add('hide'), {once: true});
+    
     selector.classList.remove('hide');
 };
